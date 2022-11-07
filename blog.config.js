a9a0c0f17d308217e0f28f3ebfb2cfd0c195b01e
AkibaRika <lxclxc89816@gmail.com>
2022-11-08 11:55:00 +1300

    init comment config

--- blog.config.js
+++ blog.config.js
@@ -50,7 +50,7 @@ const BLOG = {
       repo: ''
     },
     cusdisConfig: {
-      appId: '', // data-app-id
+      appId: 'cf7e8ba3-72d4-4e91-bf73-9cdf83072818', // data-app-id
       host: 'https://cusdis.com', // data-host, change this if you're using self-hosted version
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
